be50d527e4e2ba022aa36c57cb91c477cd1312e3
watchoutformike <91094136+watchoutformike@users.noreply.github.com>
2022-07-01 16:16:36 -0400

    Update Header.tsx
    Added link for exchanging Pizza Tokens.

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,6 +20,17 @@ export default function Header() {
           </a>
         </div>
       </div>
+      <div className={styles.left2}>
+        <div>
+          <a
+            href="/"
+            rel="noopener noreferrer"
+            onClick={() => window.open("https://quickswap.exchange/#/swap?outputCurrency=0x6b252602d7d6740a62a1eabe6f0408cfb6995d42", "_blank")}
+          >
+            <img src={`/logo2.png`} alt="Pizza Token Logo" width={80} />
+          </a>
+        </div>
+      </div>
       <div className={styles.right}>
         {address ? (
           <>
@@ -27,6 +38,7 @@ export default function Header() {
               className={styles.secondaryButton}
               onClick={() => disconnectWallet()}
             >
+              
               Disconnect Wallet
             </a>
             <p style={{ marginLeft: 8, marginRight: 8, color: "grey" }}>|</p>
@@ -44,3 +56,4 @@ export default function Header() {
     </div>
   );
 }
+
